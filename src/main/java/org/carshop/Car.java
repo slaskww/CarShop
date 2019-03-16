6b2aa83ab0ce1257142587cf1485cb33cac757c7
lukasz <slaskww@gmail.com>
2019-03-16 13:07:49 +0100

    Add menu layout in  class Shop

--- src/main/java/org/carshop/Car.java
+++ src/main/java/org/carshop/Car.java
@@ -1,6 +1,46 @@
 package org.carshop;
 
+import java.math.BigDecimal;
+
 public class Car {
 
+private String color;
+private String make;
+private BigDecimal price;
+private int door;
+private int year;
+private int milage;
 
+    public Car(String color, String make, BigDecimal price, int door, int year, int milage) {
+        this.color = color;
+        this.make = make;
+        this.price = price;
+        this.door = door;
+        this.year = year;
+        this.milage = milage;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public int getDoor() {
+        return door;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getMilage() {
+        return milage;
+    }
 }
---
+++ src/main/java/org/carshop/Shop.java
@@ -0,0 +1,64 @@
+package org.carshop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shop {
+
+    public static void main(String[] args) {
+
+
+        List<Car> carList = new ArrayList<Car>();
+
+        showTitle();
+        showMainMenu();
+       // showListMenu();
+       // showSortMenu();
+       // showFilterMenu();
+    }
+
+   public static void showTitle(){
+       System.out.println("=======================");
+       System.out.println(" Car-Shop PHU Lukaszex  ");
+       System.out.println("I bedzie Pan zadowolony!");
+       System.out.println("=======================");
+   }
+
+   public static void showMainMenu(){
+       System.out.println("1 - dodaj nowy smochód");
+       System.out.println("2 - usuń samochód");
+       System.out.println("3 - wyświetl listę samochodów");
+       System.out.println();
+   }
+
+
+   public static void showListMenu(){
+       System.out.println("Jak wyświetlić listę?");
+       System.out.println("1 - posortuj i wyświetl");
+       System.out.println("2 - przefiltruj i wyświetl");
+       System.out.println();
+   }
+
+   public static void showSortMenu(){
+       System.out.println("Jak posortoać listę?");
+       System.out.println("1 -  wg. producenta");
+       System.out.println("2 -  wg. ceny");
+       System.out.println("3 -  wg. rocznika");
+       System.out.println("4 -  wg. przebiegu");
+       System.out.println("5 -  wg. liczby drzwi");
+       System.out.println("6 -  wg. koloru");
+       System.out.println();
+   }
+
+   public static void showFilterMenu(){
+       System.out.println("Jak przefiltrować listę?");
+       System.out.println("1 -  producent");
+       System.out.println("2 -  cena");
+       System.out.println("3 -  rocznik");
+       System.out.println("4 -  przebieg");
+       System.out.println("5 -  liczba drzwi");
+       System.out.println("6 -  kolor");
+       System.out.println();
+   }
+
+}
