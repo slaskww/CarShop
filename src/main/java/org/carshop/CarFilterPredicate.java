12564b98e0af2f27c2c3a8bedd6980cd8360d6e5
lukasz <slaskww@gmail.com>
2019-03-17 15:01:13 +0100

    Add class CarFilterPredicate

--- src/main/java/org/carshop/CarFilterPredicate.java
+++ src/main/java/org/carshop/CarFilterPredicate.java
@@ -1,4 +1,49 @@
 package org.carshop;
 
+import java.util.List;
+import java.util.function.Predicate;
+
 public class CarFilterPredicate {
+
+    public static class FilterByPrice implements Predicate<Car>{
+        @Override
+        public boolean test(Car car) {
+            return car.getPrice().equals()
+        }
+    }
+
+    public static class FilterByMake implements Predicate<Car>{
+        @Override
+        public boolean test(Car car) {
+            return false;
+        }
+    }
+
+    public static class FilterByYear implements Predicate<Car>{
+        @Override
+        public boolean test(Car car) {
+            return false;
+        }
+    }
+
+    public static class FilterByMileage implements Predicate<Car>{
+        @Override
+        public boolean test(Car car) {
+            return false;
+        }
+    }
+
+    public static class FilterByDoor implements Predicate<Car>{
+        @Override
+        public boolean test(Car car) {
+            return false;
+        }
+    }
+
+    public static class FilterByColor implements Predicate<Car>{
+        @Override
+        public boolean test(Car car) {
+            return false;
+        }
+    }
 }
